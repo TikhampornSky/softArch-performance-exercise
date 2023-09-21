bd383df8402ee2c6cb943de5d1b29016f1a1a511
TikhampornSky <tikhamporntan@gmail.com>
2023-09-21 20:29:07 +0700

    measure tasks

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 var (
-	numberOfExperiment = 2000
+	duration = 1 * time.Minute
 )
 
 func main() {
@@ -34,7 +34,8 @@ func measureRabbitMQ(ch1 chan string) {
 
 	client := &http.Client{}
 	startTime := time.Now()
-	for i := 0; i < numberOfExperiment; i++ {
+	iteration := 0
+	for {
 		req, err := http.NewRequest("POST", apiURL, bytes.NewBuffer(requestBody))
 		if err != nil {
 			fmt.Println("Error creating request:", err)
@@ -55,43 +56,40 @@ func measureRabbitMQ(ch1 chan string) {
 			return
 		}
 		_ = body
+
+		iteration++
+		if time.Since(startTime) >= duration {
+			break
+		}
 	}
-	elapsedTime := time.Since(startTime)
-	fmt.Println("Total time for RabbitMQ:", elapsedTime)
+	fmt.Println("Number of task RabbitMQ can produce:", iteration)
 	ch1 <- "Done"
 }
 
 func measureKafka(ch2 chan string) {
 	fmt.Println("Testing Kafka")
-	apiURL1 := "http://localhost:8081/produce"
-	// apiURL2 := "http://localhost:8081/consume"
+	apiURL := "http://localhost:8081/produce"
 
-	requestBody1 := []byte(`{
+	requestBody := []byte(`{
 		"config": {
 		  "retries": 0
 		},
 		"message": "Hello World!",
 		"topic": "Italian"
 	}`)
-	// requestBody2 := []byte(`{
-	// 	"config": {
-	// 	  "auto.offset.reset": "earliest"
-	// 	},
-	// 	"group_id": "Kitchen 1",
-	// 	"topic": "Italian"
-	// }`)
 
-	client2 := &http.Client{}
-	startTime2 := time.Now()
-	for i := 0; i < numberOfExperiment; i++ {
-		req, err := http.NewRequest("POST", apiURL1, bytes.NewBuffer(requestBody1)) // Produce
+	client := &http.Client{}
+	startTime := time.Now()
+	iteration := 0
+	for {
+		req, err := http.NewRequest("POST", apiURL, bytes.NewBuffer(requestBody))
 		if err != nil {
 			fmt.Println("Error creating request:", err)
 			return
 		}
 		req.Header.Set("Content-Type", "application/json")
 
-		resp, err := client2.Do(req)
+		resp, err := client.Do(req)
 		if err != nil {
 			fmt.Println("Error making request:", err)
 			return
@@ -105,28 +103,11 @@ func measureKafka(ch2 chan string) {
 		}
 		_ = body
 
-		// req2, err := http.NewRequest("POST", apiURL2, bytes.NewBuffer(requestBody2)) // Consume
-		// if err != nil {
-		// 	fmt.Println("Error creating request:", err)
-		// 	return
-		// }
-		// req2.Header.Set("Content-Type", "application/json")
-
-		// resp2, err := client2.Do(req2)
-		// if err != nil {
-		// 	fmt.Println("Error making request:", err)
-		// 	return
-		// }
-		// defer resp2.Body.Close()
-
-		// body2, err := io.ReadAll(resp2.Body)
-		// if err != nil {
-		// 	fmt.Println("Error reading response body:", err)
-		// 	return
-		// }
-		// _ = body2
+		iteration++
+		if time.Since(startTime) >= duration {
+			break
+		}
 	}
-	elapsedTime2 := time.Since(startTime2)
-	fmt.Println("Total time for Kafka:", elapsedTime2)
+	fmt.Println("Number of task Kafka can produce:", iteration)
 	ch2 <- "Done"
 }
